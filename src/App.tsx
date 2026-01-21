826612c34bc97e473337225fc7120ff08765be98
thst1025 <thst0125@gmail.com>
2026-01-21 09:48:56 +0100

    endret path til bildene

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import type { BackgroundType } from "./story/types";
 import "./styles.css";
 
 const backgroundMap: Record<BackgroundType, string> = {
-  train: "/bg/train.jpg",
-  conversation: "/bg/conversation.jpg",
-  regret: "/bg/regret.jpg",
-  control: "/bg/control.jpg",
-  exit: "/bg/exit.jpg",
-  ending: "/bg/ending.jpg",
+  train: "bg//train.jpg",
+  conversation: "bg//conversation.jpg",
+  regret: "bg//regret.jpg",
+  control: "bg//control.jpg",
+  exit: "bg//exit.jpg",
+  ending: "bg//ending.jpg",
 };
 
 function App() {
